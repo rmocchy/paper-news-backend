3a89ca8234e94e35aa8d5eaf33f4aec93ae251a7
mozuku55 <ryo.tako.05@gmail.com>
2024-11-26 22:17:08 +0900

    chore: first commit

---
+++ bin/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then
+  exec /usr/local/bin/aws-lambda-rie "$@"
+else
+  exec "$@"
+fi
---
+++ src/go.mod
@@ -0,0 +1,5 @@
+module paper-news-backend
+
+go 1.23.3
+
+require github.com/aws/aws-lambda-go v1.47.0 // indirect
